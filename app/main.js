f44ea90b99c331862ef56fddf4c0950c84daa768
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-09-14 18:27:45 +0000

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -6,6 +6,13 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+    connection.on("data", (data) => {
+        const str = data.toString();
+        console.log({data, str});
+
+        connection.write("\x00\x00\x00\x00\x00\x00\x00\x07");
+        connection.end();
+    })
 });
 
 server.listen(9092, "127.0.0.1");
